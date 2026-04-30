770d17b10b0b376ad2533b52928526b8495c5864
pacaunt <t.tanpakorn@gmail.com>
2026-05-01 00:17:28 +0700

    added svg editor

--- svgx/target/debug/deps/svgx-64c90e41b4b74de9.d
+++ svgx/target/debug/deps/svgx-64c90e41b4b74de9.d
@@ -1,5 +1,5 @@
-/home/pacaunt/Documents/Playground/rust/svgx/target/debug/deps/svgx-64c90e41b4b74de9.d: src/lib.rs
+/home/pacaunt/.local/share/typst/packages/local/sanor/0.2.0/svgx/target/debug/deps/svgx-64c90e41b4b74de9.d: src/lib.rs
 
-/home/pacaunt/Documents/Playground/rust/svgx/target/debug/deps/libsvgx-64c90e41b4b74de9.rmeta: src/lib.rs
+/home/pacaunt/.local/share/typst/packages/local/sanor/0.2.0/svgx/target/debug/deps/libsvgx-64c90e41b4b74de9.rmeta: src/lib.rs
 
 src/lib.rs:
